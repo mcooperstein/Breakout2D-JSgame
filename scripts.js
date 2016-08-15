865524af74ca2c1eb26b78e69bc2d8200bd7fdcf
Marc Cooperstein <mcooperstein@gmail.com>
2016-08-14 20:55:55 -0700

    created paddle
    -created user controlled paddle

--- scripts.js
+++ scripts.js
@@ -8,6 +8,31 @@ var dx = 2;
 var dy = -2;
 
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    } else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    } else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 
 function drawBall() {
     //draw ball
@@ -18,16 +43,31 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
+
 //Define Drawing Loop to update the drawing of each frame
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     //Collision system - ball bouncing off the walls
     if (x + dx < 0 + ballRadius || x + dx > canvas.width - ballRadius) {
         dx = -dx;
     }
     if (y + dy < 0 + ballRadius || y + dy > canvas.height - ballRadius) {
         dy = -dy;
+    }
+    //Controlling the paddle movement
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
     }
     x += dx;
     y += dy;
